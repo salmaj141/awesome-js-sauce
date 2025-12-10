da890905d583ed540dee855248c2549171fd75b9
Salma <salma.jabareen@mail.huji.ac.il>
2025-12-10 22:44:04 +0200

    deleted linein awesome

--- src/awesome.js
+++ src/awesome.js
@@ -2,6 +2,5 @@ export default {
   sauce: function(){
     alert('your crap website is now been awesome sauced');
     console.log('your crap website website is now awesome');
-    console.log('something extra');
   }
 }
